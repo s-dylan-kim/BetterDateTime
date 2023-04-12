ecc29852f9f65773b2a0076d44616a8c2cfd1458
sdylankim <sunghok2@illinois.edu>
2023-04-12 00:41:08 -0400

    working prototype

--- betterDateTime/index.js
+++ betterDateTime/index.js
@@ -6,15 +6,15 @@
  */
 module.exports = (Plugin, Library) => {
 
-    const {Logger} = Library;
-    
+    const {Logger, Patcher, DiscordModules} = Library;
+
     return class extends Plugin {
 
         onStart() {
             Logger.info("Plugin enabled!");
             Patcher.before(DiscordModules.MessageActions, "sendMessage", (t,a) => {
                 let content = a[1].content;
-                let regex = /\b(\d{4})(?:-?(\d{2}))?(?:-?(\d{2}))?(?:[T ](\d{1,2}):?(\d{2})(?::?(\d{2}))?(?:\.(\d{3})))?(Z|(?:[+-]\d{2}):?(?:\d{2}))?\b/g;
+                let regex = /({(\d{4})((-?\d{2}){0,2}|\d{4})?)?(({|T)\d{2}(:?\d{2})?(:?\d{2}(.\d{3})?)?)?}/g; // (Z|[+-]\d{2}(:?\d{2})?)? timezone not needed
                 if (regex.test(content)) {
                     content = content.replace(regex, this.replaceDateTime.bind(this));
                     if (content.length > 2000) {
@@ -25,8 +25,6 @@ module.exports = (Plugin, Library) => {
                     a[1].content = content;
                 }
             });
-  
-            this.update();
         }
 
         onStop() {
@@ -34,8 +32,33 @@ module.exports = (Plugin, Library) => {
             Logger.info("Plugin disabled!");
         }
 
-        replaceDateTime(match, year, month, day, hour, minute, second, millisecond, timezone, offset, string) {
-            return "<t:" + Math.floor(new Date('2012.08.10').getTime() / 1000) + ">"
+        replaceDateTime(match, date, year, month_day, month_day_hyphen, time, isCombined, minute, second_ms, ms, timezone, minute_off, string) {
+            match = match.substring(1, match.length-1); //remove {}
+
+            let flag = "";
+
+            if (date == null) {
+                let now = new Date();
+                match = "T" + match;
+                match = "-" + String(now.getDate()).padStart(2, '0') + match;
+                match = "-" + String(now.getMonth()+1).padStart(2, '0') + match;
+                match = now.getFullYear() + match;
+
+                if (second_ms != null) {
+                    flag = "T";
+                } else {
+                    flag = "t";
+                }
+            }
+            else if (time == null) {
+                match += "T00:00";
+                flag = "d";
+            }
+            else if (second_ms != null) {
+                flag = "F";
+            }
+
+            return "<t:" + Math.floor(new Date(match).getTime() / 1000) + (flag != "" ? ":" : "") + flag + ">";
         }
     };
 
